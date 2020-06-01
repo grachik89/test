1aed5238e4997928c8f0e8c9ce5d3dbd98374802
Dmitry <grachik89@gmail.com>
2020-06-01 21:31:38 +0300

    New commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('button1').onclick = function () {
+    document.getElementById('element1').style.visibility = 'hidden';
+    document.getElementById('element2').style.backgroundColor = 'yellow';
+};
